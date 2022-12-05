4b6a2a56954ac28f89324371bbc8297938b06958
KID <50666207+KID421@users.noreply.github.com>
2022-12-05 21:30:13 +0800

    Update 建立欄位腳本

--- Assets/Scripts/FirstScript.cs
+++ Assets/Scripts/FirstScript.cs
@@ -1,30 +1,42 @@
-// �����ѡA���Q�{������A�����γ~
+﻿// 單行註解，不被程式執行，說明用途
 
-// �ޥ� Unity �������R�W�Ŷ��A�禡�w�A�\��ܮw�A�}�o�C���\��
-using UnityEngine;  // �o�̤]�i�H�[
+// 引用 Unity 引擎的命名空間，函式庫，功能倉庫，開發遊戲功能
+using UnityEngine;  // 這裡也可以加
 
-// �}���A�{���٬����O�A�w�q�����ŹϡA�Ҧp�G�\�Фl���Ź�
-// �Ź��ର���󪺤覡
+// 腳本，程式稱為類別，定義物件的藍圖，例如：蓋房子的藍圖
+// 藍圖轉為物件的方式
 public class FirstScript : MonoBehaviour
 {
-    // ��z�{�� Ctrl + K D
-    // �Y�ƫ��� Tab
-    // �}�����e
+    // 整理程式 Ctrl + K D
+    // 縮排按鍵 Tab
+    // 腳本內容
 
-    // �ƥ�G�b�S�w�ɶ��I�|���檺�A�W�٬��Ŧ�
-    // ����ƥ�G����C���ɰ���@���A��l�B�z
+    // 事件：在特定時間點會執行的，名稱為藍色
+    // 喚醒事件：播放遊戲時執行一次，初始處理
     private void Awake()
     {
-        // ��X(�T��)�F
+        // 輸出(訊息)；
         print(777);
-        print("���o�A�U�w @3@");
+        print("哈囉，沃德 @3@");
     }
 
-    // �}�l�ƥ�G����ƥ�����~�|����}�l�ƥ�@��
+    // 開始事件：喚醒事件執行後才會執行開始事件一次
     private void Start()
     {
-        print("<color=yellow>�ڦb�}�l�ƥ�̭�</color>");
+        print("<color=#662244>我在開始事件裡面</color>");
+
+        // Ctrl + D 拷貝程式並換行
+        print("字串內運算 10 + 7");
+        print($"字串內運算 {10 + 7}");
+    }
+
+    // 更新事件：一秒約執行 60 次，約 60 FPS
+    // 玩家的輸入偵測，例如：鍵盤、滑鼠、搖桿、觸控
+    // 物件持續行為，例如：移動、旋轉、縮放
+    private void Update()
+    {
+        print("<color=#ff0066>我是更新事件！</color>");
     }
 }
 
-// FirstScript �}���~
+// FirstScript 腳本外
