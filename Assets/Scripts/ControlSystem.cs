e9fdb641ed3793dec92cf416d167acb0141c3257
KID <50666207+KID421@users.noreply.github.com>
2023-01-18 21:32:00 +0800

    Update 發射彈珠

--- Assets/Scripts/ControlSystem.cs
+++ Assets/Scripts/ControlSystem.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 namespace KID
 {
@@ -7,6 +8,7 @@ namespace KID
     /// </summary>
     public class ControlSystem : MonoBehaviour
     {
+        #region 資料
         [SerializeField, Header("旋轉速度"), Range(0, 100)]
         private float speedTurn = 10.5f;
         [SerializeField, Header("可發射彈珠數量"), Range(0, 100)]
@@ -19,6 +21,42 @@ namespace KID
         private GameObject prefabMarble;
 
         private string parAttack = "觸發攻擊";
+        #endregion
+
+        [SerializeField, Header("生成彈珠的位置")]
+        private Transform pointSpawnMarble;
+
+        #region 事件
+        private void Awake()
+        {
+            StartCoroutine(ShootMarble());
+        }
+
+        private void Update()
+        {
+            
+        }
+        #endregion
+
+        #region 方法
+        /// <summary>
+        /// 發射彈珠
+        /// </summary>
+        private IEnumerator ShootMarble()
+        {
+            for (int i = 0; i < countMarbleShoot; i++)
+            {
+                // 暫存彈珠 = 生成物件(要生成的物件，座標，角度)
+                // Quaternion.identity 零度角
+                GameObject tempMarble = Instantiate(prefabMarble, pointSpawnMarble.position, Quaternion.identity);
+
+                // 暫存彈珠.取得元件<剛體>().添加推力(X, Y, Z);
+                tempMarble.GetComponent<Rigidbody>().AddForce(0, 0, speedMarble);
+                
+                yield return new WaitForSeconds(fireInterval);
+            }
+        }
+        #endregion
     }
 }
 
