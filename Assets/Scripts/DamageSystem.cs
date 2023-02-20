ff668f135e7b878c0b334bc0e09711560200fdba
KID <50666207+KID421@users.noreply.github.com>
2023-02-20 21:29:03 +0800

    Update 設定血條縮減效果

---
+++ Assets/Scripts/DamageSystem.cs
@@ -0,0 +1,32 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace KID
+{
+    /// <summary>
+    /// 受傷系統
+    /// 受到傷害、執行動畫、彈出傷害值、死亡處理
+    /// </summary>
+    public class DamageSystem : MonoBehaviour
+    {
+        [SerializeField, Header("基本資料")]
+        private DataBasic data;
+        [SerializeField, Header("血條")]
+        private Image imgHp;
+        [SerializeField, Header("畫布傷害值")]
+        private GameObject prefabDamage;
+        [SerializeField, Header("文字血量")]
+        private TextMeshProUGUI textHp;
+
+        private float hp, hpMax;
+
+        private void Awake()
+        {
+            hp = data.hp;
+            hpMax = hp;
+
+            textHp.text = hp.ToString();
+        }
+    }
+}
