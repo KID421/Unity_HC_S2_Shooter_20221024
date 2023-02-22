f3baed36709583584cbba80c74f5031102f58e69
KID <50666207+KID421@users.noreply.github.com>
2023-02-22 20:12:34 +0800

    Update 完成基本傷害功能

--- Assets/Scripts/DamageSystem.cs
+++ Assets/Scripts/DamageSystem.cs
@@ -18,14 +18,51 @@ namespace KID
         private GameObject prefabDamage;
         [SerializeField, Header("文字血量")]
         private TextMeshProUGUI textHp;
+        [SerializeField, Header("動畫控制器")]
+        private Animator ani;
 
         private float hp, hpMax;
+        private string parDamage = "觸發受傷";
 
         private void Awake()
         {
             hp = data.hp;
             hpMax = hp;
 
+            textHp.text = hp.ToString();
+
+            Damage(80);
+            Damage(80);
+        }
+
+        /// <summary>
+        /// 受傷
+        /// </summary>
+        /// <param name="attck">接收到的攻擊力</param>
+        public void Damage(float attck)
+        {
+            hp -= attck;
+            textHp.text = hp.ToString();
+            imgHp.fillAmount = hp / hpMax;
+            ani.SetTrigger(parDamage);
+
+            // transform.position 此物件的座標
+            // Quaternion.Euler(0, 0, 0) 指定角度
+            GameObject tempDamage = Instantiate(prefabDamage, transform.position, Quaternion.Euler(60, 0, 0));
+
+            // transform.GetChild(編號) 取得子物件
+            // GetComponent<元件>() 取得元件
+            tempDamage.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "-" + attck;
+
+            if (hp <= 0) Dead();
+        }
+
+        /// <summary>
+        /// 死亡
+        /// </summary>
+        private void Dead()
+        {
+            hp = 0;
             textHp.text = hp.ToString();
         }
     }
---
+++ Assets/Scripts/DataEnemy.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace KID
+{
+    /// <summary>
+    /// 敵人資料
+    /// </summary>
+    // : 後面指的是繼承的類別，繼承後會擁有該類別的資料
+    public class DataEnemy : DataBasic
+    {
+
+    }
+}
