0cb60cc177cafa7923ce662fd38dc280ceb7683b
KID <50666207+KID421@users.noreply.github.com>
2023-02-22 21:10:06 +0800

    Update 添加敵人受傷系統

---
+++ Assets/Scripts/DamageEnemy.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace KID
+{
+    /// <summary>
+    /// 敵人受傷系統
+    /// </summary>
+    public class DamageEnemy : DamageSystem
+    {
+
+    }
+}
--- Assets/Scripts/DataEnemy.cs
+++ Assets/Scripts/DataEnemy.cs
@@ -6,8 +6,14 @@ namespace KID
     /// 敵人資料
     /// </summary>
     // : 後面指的是繼承的類別，繼承後會擁有該類別的資料
+    [CreateAssetMenu(menuName = "KID/Data Enemy", fileName = "New Data Enemy")]
     public class DataEnemy : DataBasic
     {
-
+        [Header("金幣物件")]
+        public GameObject prefabCoin;
+        [Header("金幣數量"), Range(0, 500)]
+        public int coinCount;
+        [Header("掉落機率"), Range(0f, 1f)]
+        public float coinProbability;
     }
 }
